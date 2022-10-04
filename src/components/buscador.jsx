a7b3b6f9ab50275d62d712414bb1bf76e8259605
gero <geronicola1696@gmail.com>
2022-10-03 22:12:46 -0300

    comienzo header

---
+++ src/components/buscador.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Buscador = () => {
+    return (
+        <>
+            <header> 
+                <div className="logo-container">
+                    <img className="img-logo" src="./images/Pokeball.png" alt="" />
+                    <h1> Pokedex </h1>
+                </div>
+                <div className="header-actions"> 
+                    <span>#</span>
+                    <svg className="icono-flecha" xmlns="http://www.w3.org/2000/svg" width="14" height="20" fill="none" viewBox="0 0 10 16">
+                        <path fill="#212121" d="m9.017 11.108-.252-.252a.429.429 0 0 0-.606 0l-2.98 2.997V.43A.429.429 0 0 0 4.75 0h-.357a.429.429 0 0 0-.429.429v13.424l-2.98-2.997a.429.429 0 0 0-.606 0l-.252.252a.429.429 0 0 0 0 .606l4.142 4.16a.429.429 0 0 0 .607 0l4.142-4.16a.429.429 0 0 0 0-.606Z"/>
+                    </svg>
+
+                </div>
+           
+            </header>
+        </>
+    )
+}
+
+export default Buscador
