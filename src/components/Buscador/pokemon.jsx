28440c9da7f8966c502b00e45aa86e67cb570e72
joaccocaballero <joaquincaballero182@gmail.com>
2022-10-04 20:27:23 -0300

    se termina pokemon comp

--- src/components/Buscador/pokemon.jsx
+++ src/components/Buscador/pokemon.jsx
@@ -5,25 +5,7 @@ const Pokemon = (props) =>{
         <>        
         <div className="pokemon-item">
             <div className="pokemon-art">
-                <span className="id">2121</span>
-                <img src="./images/squirtle.png"/>
-            </div>
-            <div className="pokemon-name">
-                <p>Squirtle</p>
-            </div>
-        </div>
-        <div className="pokemon-item">
-            <div className="pokemon-art">
-                <span className="id"></span>
-                <img src="./images/squirtle.png"/>
-            </div>
-            <div className="pokemon-name">
-                <p>Squirtle</p>
-            </div>
-        </div>
-        <div className="pokemon-item">
-            <div className="pokemon-art">
-                <span className="id"></span>
+                <span className="id">#2121</span>
                 <img src="./images/squirtle.png"/>
             </div>
             <div className="pokemon-name">
